3e5baeca5b4db3815c5d2f2b0de7647aeb9cc915
wwh <847666794www@163.com>
2024-12-31 17:48:52 +0800

    测试

--- src/utils/three/initThree.js
+++ src/utils/three/initThree.js
@@ -1,30 +1,44 @@
-import * as THREE from 'three';
+import * as THREE from "three";
 export default class initThree {
   constructor(canvas) {
     //场景
     this.scene = new THREE.Scene();
-    this.camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000000);
+    this.camera = new THREE.PerspectiveCamera(
+      45,
+      canvas.offsetWidth / canvas.offsetHeight,
+      1,
+      1000000
+    );
     this.camera.position.set(0, 0, 250);
     // this.camera.lookAt(0,0,0)
     this.camera.updateProjectionMatrix();
     //将场景中的物体
     this.renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
-    this.renderer.setSize(canvas.clientWidth, canvas.clientHeight);
+    this.renderer.setSize(canvas.offsetWidth, canvas.offsetHeight);
+    // this.renderer.setSize(canvas.innerWidth, canvas.innerHeight);
     //设置像素比,将他设置为设备像素比
     this.renderer.setPixelRatio(canvas.devicePixelRatio);
-    this.renderer.setClearColor('#add8e6');
+    this.renderer.setClearColor("#add8e6");
 
     this.renderer.shadowMap.enabled = true;
+
+    // window.addEventListener("resize", () => {
+    //   this.camera.aspect = canvas.offsetWidth / canvas.offsetHeight;
+    //   this.camera.updateProjectionMatrix();
+    //   this.renderer.setSize(canvas.offsetWidth, canvas.offsetHeight);
+    // });
   }
   update() {
     this.renderer.render(this.scene, this.camera);
   }
   //自适应
-  resize() {
-    this.camera.aspect = window.innerWidth / window.innerHeight;
-    this.camera.updateProjectionMatrix();
-    this.renderer.setSize(window.innerWidth, window.innerHeight);
-  }
+  // resize(canvas) {
+  //   // this.renderer.setSize(canvas.offsetWidth, canvas.offsetHeight);
+  //   // this.camera.aspect = canvas.offsetWidth / canvas.offsetHeight;
+  //   this.camera.aspect = canvas.offsetWidth / canvas.offsetHeight;
+  //   this.camera.updateProjectionMatrix();
+  //   this.renderer.setSize(canvas.offsetWidth, canvas.offsetHeight);
+  // }
   //添加环境光
   addAmbientLight(intensity = 1, color = 0xffffff) {
     // 环境光
--- src/views/pcd/index.vue
+++ src/views/pcd/index.vue
@@ -1,13 +1,14 @@
 <template>
     <div class="canvasbox">
         <a-space>
-            <a-button type="primary" @click="togglePencilMode">按钮</a-button>
+            <!-- <a-button type="primary" @click="togglePencilMode">按钮</a-button>
             <a-button type="primary" @click="saveCanvas">保存</a-button>
             <a-button type="primary" @click="clearCanvas">清除</a-button>
             <a-button type="primary" @click="prevImage">上一张</a-button>
-            <a-button type="primary" @click="nextImage">下一张</a-button>
+            <a-button type="primary" @click="nextImage">下一张</a-button> -->
         </a-space>
-        <canvas ref="canvasDom" class="canvas"></canvas>
+        <canvas ref="canvasDom" class="canvas">
+        </canvas>
     </div>
 
 
@@ -21,8 +22,12 @@
     import * as THREE from "three";
     import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
     import { PCDLoader } from 'three/addons/loaders/PCDLoader.js';
+    import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
     import { clearScene, disposeChild } from "@/utils/three/clearScene.js";
-    const canvasDom = ref(null);
+
+    import { Pathfinding, PathfindingHelper } from 'three-pathfinding';
+
+    let canvasDom = ref(null);
 
     let baseThree, controls;
 
@@ -30,20 +35,66 @@
     // 创建一个组并添加多个立方体
     group.value = new THREE.Group();
 
+    let objMesh;
     // 加载点云模型
-    let model = ref('/models/pcd/test.pcd')
+    // let model = ref('/models/pcd/test.pcd')
+    let model = ref('/models/pcd/pcl_logo.pcd')
+    // let model = ref('/models/WoodTower/Wood_Tower.glb')
     // 加载点云
-    function loadModel(model, scaleX, scaleY, scaleZ) {
+    function loadModel(model, scaleX, scaleY, scaleZ, rotationX, rotationY, rotationZ, positionX, positionY, positionZ) {
         const loader = new PCDLoader();
+        // const loader = new GLTFLoader();
         loader.load(model, function (obj) {
             obj.geometry.center();
-            obj.geometry.rotateX(-Math.PI / 2);
+
             obj.name = 'pcd';
             obj.scale.set(scaleX, scaleY, scaleZ);
+            obj.rotation.set(rotationX, rotationY, rotationZ);
+            obj.position.set(positionX, positionY, positionZ);
 
             group.value.add(obj);
             baseThree.scene.add(obj);
             baseThree.scene.add(group.value);
+
+
+        });
+    }
+
+    let playerPosition = new THREE.Vector3(10, 10, 10);
+    let targetPosition = new THREE.Vector3(5, 8, 7);
+    const pathfinding = new Pathfinding();
+    const helper = new PathfindingHelper();
+
+    function loadGltfModel(model, scaleX, scaleY, scaleZ, rotationX, rotationY, rotationZ, positionX, positionY, positionZ) {
+        const loader = new GLTFLoader();
+        loader.load(model, function (obj) {
+
+            obj.scene.traverse((node) => {
+                if (node.isMesh) {
+                    objMesh = node
+                    console.log(`output->@@@666objMesh`, objMesh)
+                    // Create level.
+                    const ZONE = 'level1';
+                    pathfinding.setZoneData(ZONE, Pathfinding.createZone(objMesh.geometry));
+
+                    // Find path from A to B.
+                    const groupID = pathfinding.getGroup(ZONE, playerPosition);
+                    const path = pathfinding.findPath(playerPosition, targetPosition, ZONE, groupID);
+
+                };
+
+            });
+
+
+            obj.scene.name = 'pcd';
+            obj.scene.scale.set(scaleX, scaleY, scaleZ);
+            obj.scene.rotation.set(rotationX, rotationY, rotationZ);
+            obj.scene.position.set(positionX, positionY, positionZ);
+
+            group.value.add(obj.scene);
+            baseThree.scene.add(obj.scene);
+            baseThree.scene.add(group.value);
+
 
         });
     }
@@ -55,22 +106,42 @@
     onMounted(() => {
         console.log(`output->canvasDom.value`, canvasDom.value)
         baseThree = new initThree(canvasDom.value);
-        baseThree.camera.position.set(30, 30, 30);
+        baseThree.camera.position.set(10, 10, 10);
         baseThree.initAxesHelper();
-        loadModel(model.value, 10, 10, 10)
+        baseThree.addAmbientLight()
+        baseThree.addDirLight()
+
+        baseThree.scene.add(helper);
+        loadModel(model.value, 1, 1, 1, -Math.PI / 2, 0, 0, 1, 1, 1);
+        // loadGltfModel(model.value, 1, 1, 1, 0, 0, 0, 1, 1, 1);
+
+
+
+
         controls = new OrbitControls(baseThree.camera, baseThree.renderer.domElement);
         controls.enableDamping = true//开启阻尼,让动画更平滑
-        update();
+        animate();
+
+
+
+        window.addEventListener("resize", resize);
 
     });
 
     let i = 0
-    function update() {
+    function animate() {
         i++
-        requestAnimationFrame(update);
-        console.log(`output->i@@@`, i)
+        requestAnimationFrame(animate);
+        // console.log(`output->i@@@`, i)
         baseThree.update();
         controls.update();
+    }
+
+
+    function resize() {
+        console.log(`output->窗口变化`, canvasDom.value.offsetWidth)
+        // let canvasDom = ref(null);
+        baseThree.resize(canvasDom.value)
     }
 
 
@@ -91,10 +162,9 @@
     .canvasbox {
         width: 100%;
         height: 100%;
-        display: flex;
-        flex-direction: column;
-        align-items: center;
         position: relative;
+        display: flex;
+        box-sizing: border-box;
     }
 
     .ant-space {
@@ -106,7 +176,7 @@
     .canvas {
         width: 100%;
         height: 100%;
-        overflow: hidden;
+        border: 5px solid #000;
     }
 
 </style>
